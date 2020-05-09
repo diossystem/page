51db8bbb35ce2a8acbb990f488c11c1baea439f0
Oleg Dmitrochenko <dmitrochenkooleg@gmail.com>
2020-05-09 22:07:41 +0300

    change seeds: add pages to the Map AF

--- database/seeds/AdditionalFieldsTableSeeder.php
+++ database/seeds/AdditionalFieldsTableSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use Dios\System\Page\Models\AdditionalField;
+use Dios\System\Page\Models\Page;
 
 class AdditionalFieldsTableSeeder extends Seeder
 {
@@ -12,37 +13,63 @@ class AdditionalFieldsTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(AdditionalField::class)->create([
-            'code_name' => 'map',
-            'title' => 'Map',
-            'type' => 'map',
-        ]);
+        $this->createMap();
 
-        factory(AdditionalField::class)->create([
+        $images = factory(AdditionalField::class)->create([
             'code_name' => 'images',
             'title' => 'Images',
             'type' => 'files',
         ]);
 
-        factory(AdditionalField::class)->create([
+        $contacts = factory(AdditionalField::class)->create([
             'code_name' => 'contacts',
             'title' => 'Contacts',
         ]);
 
-        factory(AdditionalField::class)->create([
+        $sources = factory(AdditionalField::class)->create([
             'code_name' => 'sources',
             'title' => 'Sources of a content',
         ]);
 
-        factory(AdditionalField::class)->create([
+        $recommendations = factory(AdditionalField::class)->create([
             'code_name' => 'recommendations',
             'title' => 'Page with services',
             'type' => 'local_pages',
         ]);
 
-        factory(AdditionalField::class, 5)->create([
-            'active' => false,
-            'description' => 'inactive',
+        factory(AdditionalField::class, 5)
+            ->create([
+                'active' => false,
+                'description' => 'inactive',
+            ])
+            ->each(function ($af) {
+                $af->pages()->save(factory(Page::class)->make());
+            })
+        ;
+    }
+
+    public function createMap(): AdditionalField
+    {
+        $map = factory(AdditionalField::class)->create([
+            'code_name' => 'map',
+            'title' => 'Map',
+            'type' => 'map',
         ]);
+
+        $map->pages()->attach(
+            factory(Page::class)->create(['title' => 'Page with map'])->id,
+            [
+                'values' => [
+                    'title' => 'This is a map',
+                    'address' => '210000, Vitebsk, Belarus',
+                    'phone' => '80212000000',
+                    'script' => 'Here must be Javascript that contains the map.',
+                    'link' => 'Link to the map',
+                    'image_url' => 'URL to an image contains the map'
+                ],
+            ]
+        );
+
+        return $map;
     }
 }
