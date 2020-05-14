3616766d022566966f129477208c207e4e2dc4bf
Oleg Dmitrochenko <dmitrochenkooleg@gmail.com>
2020-05-14 21:43:22 +0300

    implement Map

--- src/Models/HandlersOfAdditionalFields/Map.php
+++ src/Models/HandlersOfAdditionalFields/Map.php
@@ -2,12 +2,15 @@
 
 namespace Dios\System\Page\Models\HandlersOfAdditionalFields;
 
+use Exception;
 use Dios\System\Page\Models\AdditionalFieldsOfPages;
+use Illuminate\Database\Eloquent\Model;
+use Dios\System\Page\MulticastingInterfaces\InstanceFromModel;
 
 /**
- *
+ * Keeps data of a map.
  */
-class Map
+class Map implements InstanceFromModel
 {
     protected $title;
 
@@ -15,21 +18,110 @@ class Map
 
     protected $script;
 
-    protected $link;
-
     protected $url;
 
-    function __construct(AdditionalFieldsOfPages $af, string $property)
+    protected $image;
+
+    function __construct(Model $af, string $property)
     {
+        if (! ($af instanceof AdditionalFieldsOfPages)) {
+            throw new Exception('The given instance is invalid.');
+        }
+
         $this->fillFromArray($af->$property ?? []);
     }
 
+    /**
+     * Fills an instance of the class by with values.
+     *
+     * @param  array  $values
+     */
     public function fillFromArray(array $values)
     {
-        // TODO должен содержать все переменные или значения по умолчанию
-        $this->values = $values;
+        $this->title = $values['title'] ?? 'Map';
+        $this->address = $values['address'] ?? null;
+        $this->phone = $values['phone'] ?? null;
+        $this->phones = isset($values['phones']) && is_array($values['phones']) ? $values['phones'] : [];
+        $this->script = $values['script'] ?? null;
+        $this->url = $values['url'] ?? null;
+        $this->image = $values['image'] ?? null;
     }
 
+    /**
+     * Returns a title of the map.
+     *
+     * @return string|null
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Returns an address of the point.
+     *
+     * @return string|null
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    /**
+     * Returns a script to show map.
+     *
+     * @return string|null
+     */
+    public function getScript()
+    {
+        return $this->script;
+    }
+
+    /**
+     * Returns an URL to the map.
+     *
+     * @return string|null
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    /**
+     * Returns an URL to an image of the map.
+     *
+     * @return string|null
+     */
+    public function getUrlToImage()
+    {
+        return $this->image;
+    }
+
+    /**
+     * Returns a phone number of a company.
+     *
+     * @return string|null
+     */
+    public function getPhone()
+    {
+        return $this->phone;
+    }
+
+    /**
+     * Returns phone numbers of a company.
+     *
+     * @return array
+     */
+    public function getPhones()
+    {
+        return $this->phones;
+    }
+
+    /**
+     * Returns values of the instance in the form of the array.
+     *
+     * @return array
+     */
     public function toArray(): array
     {
         return [
@@ -38,7 +130,7 @@ class Map
             'phone' => $this->phone,
             'phones' => $this->phones,
             'script' => $this->script,
-            'link' => $this->link,
+            'url' => $this->url,
             'image' => $this->image,
         ];
     }
