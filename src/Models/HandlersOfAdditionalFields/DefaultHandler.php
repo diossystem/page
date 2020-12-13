c55e5113a77d611c3c0a9d4487b5a0956cb565da
Oleg Dmitrochenko <dmitrochenkooleg@gmail.com>
2020-12-13 17:09:31 +0300

    fix path to interface

--- src/Models/HandlersOfAdditionalFields/DefaultHandler.php
+++ src/Models/HandlersOfAdditionalFields/DefaultHandler.php
@@ -2,23 +2,37 @@
 
 namespace Dios\System\Page\Models\HandlersOfAdditionalFields;
 
-use Dios\System\Multicasting\Multicasting\SimpleArrayEntity;
+use Dios\System\Multicasting\Interfaces\SimpleArrayEntity;
 
 /**
- *
+ * The default handler of pages.
  */
 class DefaultHandler implements SimpleArrayEntity
 {
+    /**
+     * Initializes an instance of the class.
+     */
     function __construct(array $values = null)
     {
         $this->fillFromArray($values ?? []);
     }
 
+    /**
+     * Fills the instance with the given array.
+     *
+     * @param array $values
+     * @return void
+     */
     public function fillFromArray(array $values)
     {
         $this->values = $values;
     }
 
+    /**
+     * Returns original values of the instance in the form of the array.
+     *
+     * @return array
+     */
     public function toArray(): array
     {
         return $this->values;
